0ae007e69b9d7d2b80753a7947d1743b3b27d299
Noah <lauffern@gmail.com>
2022-02-21 18:15:39 +0000

    New templates for headers and URL params

--- express_server.js
+++ express_server.js
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
