68169ec65fe48c2723347a9c27ce0e7935142a6e
Noah <lauffern@gmail.com>
2022-02-22 13:42:00 +0000

    updated comments, linting and delete URLS init

--- express_server.js
+++ express_server.js
@@ -10,39 +10,44 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
+//Index Page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Create new shortURL page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//Newly Generated display shortURL page
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; // making sure to set longURL as a value to key of shortURL
-  res.render("urls_show", templateVars); 
+  res.render("urls_show", templateVars); // showing HTML from urls_show with populated longURL from the form
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
+//Hyperlink to re-direct client to original longURL landing-page
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// Log the POST request body to the console
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString() 
-  let longURL = req.body.longURL
-  urlDatabase[shortURL] = longURL
-  // Log the POST request body to the console
-  // console.log(longURL);
-  // console.log(shortURL);
-  // console.log(urlDatabase)
-  res.redirect(`/urls/${shortURL}`)
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+  
+});
+
+//Delete Row
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+
+  res.redirect(`/urls`);
   
 });
 
@@ -53,8 +58,8 @@ app.listen(PORT, () => {
 
 function generateRandomString() {
 
-  let shortURL = Math.random().toString(36).substring(2,7);
+  let shortURL = Math.random().toString(36).substring(2,8);
   return shortURL;
 
-};
+}
 
