d35038eb3febbec0f9a7d35d628dbe9d74b056ff
Noah <lauffern@gmail.com>
2022-02-24 22:40:34 +0000

    more linting

--- express_server.js
+++ express_server.js
@@ -21,7 +21,7 @@ app.set("view engine", "ejs");
 const urlDatabase = {};
 const users = {};
 
-
+//Home page 
 app.get("/", (req, res) => {
   if (!req.session.user_id) {
     return res.redirect("/login");
